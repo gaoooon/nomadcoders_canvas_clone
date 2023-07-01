b1e25b6ba74755b1c8005c7c640b6b426121dc9d
gaoooon <gaon9749@gmail.com>
2023-07-01 13:43:55 +0900

    기능 완성

--- app.js
+++ app.js
@@ -1,66 +1,31 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const lineWidth = document.querySelector("#line-width");
+const colors = document.querySelector("#color");
+const colorOption = Array.from(document.getElementsByClassName("color-option"));
+const modeBtn = document.querySelector("#mode-btn");
+const destroyBtn = document.querySelector("#destroy-btn");
+const eraserBtn = document.querySelector("#eraser-btn");
+const fileInput = document.getElementById("file");
+const textInput = document.getElementById("text");
+const saveBtn = document.getElementById("save");
 
 canvas.height = 800;
 canvas.width = 800;
-// // rect는 보이진 않지만 좌표를 지정하고 지정한 크기만큰 사각형을 만드는 함수이다
-// ctx.rect(50, 50, 100, 100);
-// // fill은 지정한 사각형에 색깔을 채운다
-// ctx.fill();
-
-// ctx.beginPath();
-// // 위랑 분리해서 사용할수 있다
-// ctx.rect(150, 150, 100, 100);
-// // 사각형에 테두리만 생기게 한다
-// ctx.stroke();
-
-// ctx.beginPath();
-// ctx.rect(250, 250, 100, 100);
-// // 사각형에 채울 색깔을 지정한다
-// ctx.fillStyle = "blue";
-// ctx.fill();
-
-// ctx.beginPath();
-// ctx.rect(350, 350, 100, 100);
-// // 사각형에 태두리 색깔을 지정한다
-// ctx.strokeStyle = "red";
-// ctx.stroke();
-
-// ctx.fillRect(200, 200, 50, 200);
-// ctx.fillRect(400, 200, 50, 200);
-// ctx.lineWidth = 2;
-// ctx.strokeRect(300, 300, 50, 100);
-// ctx.fillRect(200, 200, 200, 20);
-// ctx.moveTo(200, 200);
-// ctx.lineTo(325, 100);
-// ctx.lineTo(450, 200);
-// ctx.fill();
-
-// ctx.beginPath();
-// ctx.arc(600, 300, 30, 0, 2 * Math.PI);
-// ctx.fill();
-const colors = [
-  "#ff3838",
-  "#ffb8b8",
-  "#c56cf0",
-  "#ff9f1a",
-  "#fff200",
-  "#32ff7e",
-  "#7efff5",
-];
-
-// function onClick(event) {
-//   console.log(event);
-//   ctx.beginPath();
-//   ctx.moveTo(0, 0);
-//   ctx.strokeStyle = colors[Math.floor(Math.random() * colors.length)];
-//   ctx.lineWidth = 2;
-//   ctx.lineTo(event.offsetX, event.offsetY);
-//   ctx.stroke();
-// }
-// canvas.addEventListener("mousemove", onClick);
+ctx.lineCap = "round";
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
-ctx.lineWidth = 2;
+let isFilling = false;
+
+// const colors = [
+//   "#ff3838",
+//   "#ffb8b8",
+//   "#c56cf0",
+//   "#ff9f1a",
+//   "#fff200",
+//   "#32ff7e",
+//   "#7efff5",
+// ];
 
 function onMove(event) {
   if (isPainting) {
@@ -71,15 +36,99 @@ function onMove(event) {
   ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-function onMouseDown() {
-  // ctx.lineTo(event.offsetX, event.offsetY);
+function startPainting() {
   isPainting = true;
 }
 
-function onMouseUp() {
-  // ctx.lineTo(event.offsetX, event.offsetY);
+function cancelPainting() {
   isPainting = false;
+  ctx.beginPath();
 }
+
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
+
+function onColorsChange(event) {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event) {
+  const changeValue = event.target.dataset.color;
+  ctx.strokeStyle = changeValue;
+  ctx.fillStyle = changeValue;
+  colors.value = changeValue;
+}
+
+function onModeClick() {
+  if(isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Draw";
+  } else { 
+    isFilling = true;
+    modeBtn.innerText = "Fill";
+  }
+}
+
+function onClickCanvas() {
+  if(isFilling) {
+    ctx.fillRect(0, 0, 800, 800);
+  }
+}
+
+function onDestroyClick() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, 800, 800);
+}
+
+function onEraserClick() {
+  ctx.strokeStyle = "white";
+  isFilling = false;
+}
+
+function onFileChange(event) {
+const file = event.target.files[0];
+const url = URL.createObjectURL(file);
+const image = new Image();
+image.src = url;
+image.onload = function() {
+    ctx.drawImage(image, 0, 0, 800, 800);
+    fileInput.value = null;
+  }
+}
+function onDoubleClick(event) {
+  const text = textInput.value;
+  if(text !== "") {
+  ctx.save();
+  ctx.lineWidth = 1;
+  ctx.font = "48px sans"
+  ctx.fillText(text, event.offsetX, event.offsetY);
+  ctx.restore();
+  }
+}
+
+function onSaveClick() {
+  const url = canvas.toDataURL();
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "myDrawing.png";
+  a.click();
+}
+
 canvas.addEventListener("mousemove", onMove);
-canvas.addEventListener("mousedown", onMouseDown);
-canvas.addEventListener("mouseup", onMouseUp);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onClickCanvas);
+
+lineWidth.addEventListener("change", onLineWidthChange);
+colors.addEventListener("change", onColorsChange);
+
+colorOption.forEach((color) => color.addEventListener("click", onColorClick));
+modeBtn.addEventListener("click",onModeClick);
+destroyBtn.addEventListener("click",onDestroyClick);
+eraserBtn.addEventListener("click", onEraserClick);
+fileInput.addEventListener("change", onFileChange);
+canvas.addEventListener("dblclick", onDoubleClick);
+saveBtn.addEventListener("click", onSaveClick);
